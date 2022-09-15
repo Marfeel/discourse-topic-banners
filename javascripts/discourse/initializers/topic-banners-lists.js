243912eeace461f9ab224ac6dcddbd665ee16b6d
Jonay Rodríguez <jonay.rodriguez@marfeel.com>
2022-09-15 17:17:52 +0200

    feat: topic lists banners

--- javascripts/discourse/initializers/topic-banners-lists.js
+++ javascripts/discourse/initializers/topic-banners-lists.js
@@ -1,11 +1,11 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
-  name: "topic-banners",
+  name: "topic-banners-lists",
 
   initialize() {
     withPluginApi("0.8", (api) => {
-      api.decorateWidget("topic-banners:after", (helper) => {
+      api.decorateWidget("topic-banners-lists:after", (helper) => {
         helper.widget.appEvents.on("page:changed", () => {
           helper.widget.scheduleRerender();
         });
